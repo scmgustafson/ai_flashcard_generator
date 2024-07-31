7efc2663d8186052a7395039c9600223e6b357d3
scmgustafson <scmgustafson@gmail.com>
2024-07-31 01:54:01 +0000

    Pylint fixes

--- src/main.py
+++ src/main.py
@@ -1,12 +1,8 @@
-import config
-import utilities
-
-import csv
-import json
-import logging
-
 from fastapi import FastAPI
 from openai import OpenAI
+
+import config
+import utilities
 
 app = FastAPI()
 openai_client = OpenAI(
@@ -20,7 +16,8 @@ logger = utilities.setup_logging()
 #     target_count = 50
 #     batch_count = 10
 
-#     output_dict = utilities.query_and_get_dict(target_count, batch_count, 'French vocabulary at CEFR level C1 where Side 2 is in English', output=False)
+#     output_dict = utilities.query_and_get_dict(target_count, batch_count,\
+#                                                'French vocabulary at CEFR level C1 where Side 2 is in English', output_to_file=False)
 #     return output_dict
 
 def main():
@@ -31,7 +28,8 @@ def output_dict():
     target_count = 50
     batch_count = 10
 
-    output_dict = utilities.query_and_get_dict(target_count, batch_count, 'French vocabulary at CEFR level C1 where Side 2 is in English', output=False)
+    output_dict = utilities.query_and_get_dict(target_count, batch_count, \
+                                               'French vocabulary at CEFR level C1 where Side 2 is in English', output_to_file=False)
     return output_dict
 
 if __name__ == "__main__":
@@ -41,5 +39,6 @@ if __name__ == "__main__":
 # need to add exception handling
 # retries for when openai doesnt return values
 # better query changes
-# find a way to filter out duplicates in the csv (add CSV file into query and say to generate new unique ones?)
+# find a way to filter out duplicates in the csv 
+# (add CSV file into query and say to generate new unique ones?)
 # filter out None,None in the CSV
